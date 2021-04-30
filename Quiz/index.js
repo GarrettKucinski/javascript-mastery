c2de9068ecb8384ce2117ec55d1b3261dd39d8c2
Garrett <garrett.kucinski@carvana.com>
2021-04-29 21:33:58 -0700

    add fetch for avatar api

--- Quiz/index.js
+++ Quiz/index.js
@@ -1,10 +1,21 @@
 {
-  const mainContainer = document.getElementById('quiz-container');
-  const questionContainer = document.createElement('article');
 
-  questionContainer.innerHTML = `
-    <div>This is a question</div>
-  `;
+  function createAndAppendQuestion ({ question, possibleAnsers = [] }) {
+    const mainContainer = document.getElementById('quiz-container');
+    const questionContainer = document.createElement('article');
 
-  mainContainer.appendChild(questionContainer);
+    questionContainer.innerHTML = `
+      <div>${question}</div>
+      <ul>
+        ${possibleAnsers.map(answer => `<li>${answer}<li>`).join('')}
+      </ul>
+    `;
+
+    mainContainer.appendChild(questionContainer);
+  }
+
+  const baseURL = 'https://api.sampleapis.com/avatar/questions';
+  fetch(baseURL)
+    .then(resp => resp.json())
+    .then(data => data.forEach(createAndAppendQuestion));
 }
